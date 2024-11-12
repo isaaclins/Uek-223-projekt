dc7d08b3c92ae1a0c253447062be4d61dd62c6bf
Isaac Lins <contact@isaaclins.com>
2024-11-12 12:50:49 +0100

    Create UserService.java

---
+++ src/main/java/com/isaac/service/UserService.java
@@ -0,0 +1,29 @@
+package com.isaac.service;
+
+public class UserService {
+    // Implement the methods to interact with the User entity
+    // Use the UserDAO class to interact with the database
+
+    public void createUser() {
+        // Implement the logic to create a new user
+    }
+
+    public void updateUser() {
+        // Implement the logic to update a user
+    }
+
+    public void deleteUser() {
+        // Implement the logic to delete a user
+    }
+
+    public void getUserById() {
+        // Implement the logic to get a user by ID
+    }
+
+    public void getAllUsers() {
+        // Implement the logic to get all users
+    }
+
+
+
+}
